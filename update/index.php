5c11d013c0f92995912bbb92660cab89a0105106
Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>
2014-11-11 20:26:32 +0100

    Pubs test part 1

--- update/index.php
+++ update/index.php
@@ -1,11 +1,27 @@
 ---
 layout: default
 ---
+<div class="container">
+<?php
+?>
 
+<h1>Generate publications</h1>
+<pre><code>
 <?php
 	$repository_path = '/var/www/website';
 	// $repository_path = '/Users/ken/Versioned/website';
+	include('./bibhtmler.php');
+	$bibhtmler = new bibhtmler(array('groupby' => 'year'));
+	$pubs_text = "---\nlayout: default\n---\n";
+	$pubs_text .= "<div class=\"container\">";
+	$pubs_text .= $bibhtmler->process('../../pubs/all.bib');
+	$pubs_text .= "</div>";
+	$pubs_page = fopen("../../pubs/index.html", "w");
+	fwrite($pubs_page, $pubs_text);
+	fclose($pubs_page);
+	echo("Done");
 ?>
+</code></pre>
 
 <h1>Git pull</h1>
 <pre><code>
@@ -22,11 +38,4 @@ layout: default
 	system("jekyll build --source ".$repository_path." --destination ".$repository_path."/_site 2>&1");
 ?>
 </code></pre>
-
-<h1>Printenv</h1>
-<pre><code>
-<?php
-	echo("$ printenv\n");
-	system("printenv 2>&1");
-?>
-</code></pre>
+</div>
