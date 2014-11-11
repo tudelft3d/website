db873127b93a40cee7d34c76fdd74ba013c3836c
Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>
2014-11-11 21:30:07 +0100

    Merge pubs escaping double brackets

--- update/index.php
+++ update/index.php
@@ -3,40 +3,45 @@ layout: default
 ---
 <div class="container">
 <?php
+	$repository_path = '/var/www/website';
+	// $repository_path = '/Users/ken/Versioned/website';
+	include('./bibhtmler.php');
 ?>
 
 <h1>Merging publication files</h1>
 <pre><code>
 <?php
-	$merged_bib_file = fopen("../../pubs/all.bib", "w");
 	$bib_file_paths = scandir("../../pubs");
+	$merged_bib_file_text = "";
 	foreach ($bib_file_paths as $bib_file_path) {
 		if (preg_match("/\w+.bib$/", $bib_file_path)) {
 			if ($bib_file_path === "all.bib") continue;
 			echo("Found: ".$bib_file_path."\n");
 			$bib_file = fopen("../../pubs/".$bib_file_path, "r");
-			fwrite($merged_bib_file, fread($bib_file, filesize("../../pubs/".$bib_file_path)));
+			$merged_bib_file_text .= fread($bib_file, filesize("../../pubs/".$bib_file_path));
 			fclose($bib_file);
 		}
-	} fclose($merged_bib_file);
+	} $merged_bib_file = fopen("../../pubs/all.bib", "w");
+	fwrite($merged_bib_file, $merged_bib_file_text);
+	fclose($merged_bib_file);
+	echo("Done!");
 ?>
 </code></pre>
 
 <h1>Generate publications</h1>
 <pre><code>
 <?php
-	$repository_path = '/var/www/website';
-	// $repository_path = '/Users/ken/Versioned/website';
-	include('./bibhtmler.php');
 	$bibhtmler = new bibhtmler(array('groupby' => 'year'));
 	$pubs_text = "---\nlayout: default\n---\n";
+	$pubs_text .= "{% raw %}\n";
 	$pubs_text .= "<div class=\"container\">";
 	$pubs_text .= $bibhtmler->process('../../pubs/all.bib');
 	$pubs_text .= "</div>";
+	$pubs_text .= "{% endraw %}\n";
 	$pubs_page = fopen("../../pubs/index.html", "w");
 	fwrite($pubs_page, $pubs_text);
 	fclose($pubs_page);
-	echo("Done");
+	echo("Done!");
 ?>
 </code></pre>
 
