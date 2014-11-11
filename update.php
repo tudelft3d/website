0c96c1c4e35e4176fbd192727636eb70071097c8
Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>
2014-11-11 17:17:08 +0100

    Nicer formatting in update page

--- update.php
+++ update.php
@@ -1,10 +1,24 @@
-<?php
-	$repository_path = '/var/www/website';
-	// $repository_path = '/Users/ken/Versioned/website';
+---
+layout: default
+---
 
+<?php
+	// $repository_path = '/var/www/website';
+	$repository_path = '/Users/ken/Versioned/website';
+?>
+
+<h1>Git pull</h1>
+<pre><code>
+<?php
 	$git_output = shell_exec('git pull '.$repository_path);
 	echo $git_output;
+?>
+</code></pre>
 
+<h1>Jekyll build</h1>
+<pre><code>
+<?php
 	$jekyll_output = shell_exec('jekyll build --source '.$repository_path.' --destination '.$repository_path.'/_site');
 	echo $jekyll_output;
 ?>
+</code></pre>
