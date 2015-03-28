a7ac2a317cb295586b39c3e903ab2b48f0b06c2a
Hugo Ledoux <h.ledoux@tudelft.nl>
2015-03-28 09:20:35 +0100

    okay, we're back in BK-City
    had put us in Chemical Eng building...

--- assets/js/mymap.js
+++ assets/js/mymap.js
@@ -59,9 +59,9 @@ L.CRS.proj4js = (function () {
                         }
                     )
                 ],
-                center: new L.LatLng(52.003187,4.368730), 
+                center: new L.LatLng(52.004713,4.370334), 
                 zoom: 10
             });
-            var marker = L.marker([52.003187,4.368730]).addTo(map);
+            var marker = L.marker([52.004713,4.370334]).addTo(map);
             marker.bindPopup("<b>Faculty of Architecture & the Built Environment<br>Julianalaan 134, 2628BL Delft</b><br>(our office (BG.WEST.010) is roughly where the arrow is, on the ground floor)").openPopup();
 
