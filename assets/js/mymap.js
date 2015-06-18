9e671e0b30f8e100f66accd35270d1ea38157a69
Ken Arroyo Ohori <ken@wlan-145-94-186-36.wlan.tudelft.nl>
2015-06-18 14:41:32 +0200

    Retina tiles

--- assets/js/mymap.js
+++ assets/js/mymap.js
@@ -7,7 +7,7 @@ var map = L.map('map').setView([52.004713, 4.370334], 16);
 // }).addTo(map);
 
 // Stamen toner-lite tile layer
-L.tileLayer('//stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
+L.tileLayer('//stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}' + (L.Browser.retina? '@2x': '') + '.png', {
     // attribution: '&copy; <a href="//openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
 }).addTo(map);
