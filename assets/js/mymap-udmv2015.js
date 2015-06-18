d6c8a9098b78c470a51fc5e07ccce93a9bce294b
Filip Biljecki <fbiljecki@gmail.com>
2015-06-18 15:03:17 +0200

    Moving to https and new map

--- assets/js/mymap-udmv2015.js
+++ assets/js/mymap-udmv2015.js
@@ -1,67 +1,17 @@
-L.CRS.proj4js = (function () {
-              var createProjection = function (code, def, /*L.Transformation*/ transformation) {
-                if (typeof(def) !== 'undefined') {
-                  Proj4js.defs[code] = def;
-                }
-                var proj = new Proj4js.Proj(code);
-                
-                return {
-                  project: function (latlng) {
-                    var point = new L.Point(latlng.lng, latlng.lat);
-                    return Proj4js.transform(Proj4js.WGS84, proj, point);
-                  },
+// create a map in the "map" div, set the view to a given place and zoom
+var map = L.map('map').setView([52.004713, 4.370334], 16);
 
-                  unproject: function (point, unbounded) {
-                    var point2 = Proj4js.transform(proj, Proj4js.WGS84, point.clone());
-                    return new L.LatLng(point2.y, point2.x, unbounded);
-                  }
-                };
-              };
+// OpenStreetMap tile layer
+// L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="//openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
 
-              return function (code, def, transformation) {
-                return L.Util.extend({}, L.CRS, {
-                  code: code,
-                  transformation: transformation ? transformation: new L.Transformation(1, 0, -1, 0),
-                  projection: createProjection(code, def)
-                });
-              };
-            }());
-            
-            // maak een array met de resoluties benodigd voor het tileschema:
-            var rd_res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420];
-            // maak van de projectiestring voor het Rijksdriehoekstelsel een Leaflet projectie object:
-            var rd = L.CRS.proj4js('EPSG:28992', 
-                '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 ' + 
-                '+ellps=bessel +units=m ' + 
-                '+towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs',
-                new L.Transformation(
-                    1,
-                    285401.920,
-                    -1,
-                    903401.920
-                ));
-            // Override de scale instelling voor het schema:
-            rd.scale = function(zoom) {
-                return 1 / rd_res[zoom];
-            };
-            // Kaart initialiseren:    
-            var map = new L.Map('map', {
-                continuousWorld: true,
-                crs: rd,
-                layers: [
-                    new L.TileLayer(
-                        'http://www.openbasiskaart.nl/mapcache/tms/1.0.0/osm@rd/{z}/{x}/{y}.png', {
-                            tms: true,
-                            minZoom: 1,
-                            maxZoom: 13,
-                            attribution: '© <a href="http://www.openbasiskaart.nl">openbasiskaart.nl</a>',
-                            continuousWorld: true
-                        }
-                    )
-                ],
-                center: new L.LatLng(52.004713,4.370334), 
-                zoom: 4
-            });
-            var marker = L.marker([52.004713,4.370334]).addTo(map);
-            marker.bindPopup("<b>Faculty of Architecture & the Built Environment<br>Julianalaan 134, 2628BL Delft</b>").openPopup();
+// Stamen toner-lite tile layer
+L.tileLayer('//stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}' + (L.Browser.retina? '@2x': '') + '.png', {
+    // attribution: '&copy; <a href="//openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+}).addTo(map);
+
+var marker = L.marker([52.004713,4.370334]).addTo(map);
+marker.bindPopup("<b>Faculty of Architecture & the Built Environment<br>Julianalaan 134, 2628BL Delft</b>").openPopup();
 
