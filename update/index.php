2621b3e2896aadb7bf18b2e5568ecaa6a0387657
Ken Arroyo Ohori <ken@MBP-15-de-Ken.fritz.box>
2015-07-05 21:56:00 +0200

    Shift pubs to new plugin

--- update/index.php
+++ update/index.php
@@ -67,49 +67,6 @@ layout: default
 ?>
 </code></pre>
 
-<!-- <h1>Merging publication files</h1>
-<pre><code> -->
-<?php
-	// $bib_file_paths = scandir("../../pubs");
-	// $merged_bib_file_text = "";
-	// foreach ($bib_file_paths as $bib_file_path) {
-	// 	if (preg_match("/\w+.bib$/", $bib_file_path)) {
-	// 		if ($bib_file_path === "all.bib") continue;
-	// 		echo("Found: ".$bib_file_path."\n");
-	// 		$bib_file = fopen("../../pubs/".$bib_file_path, "r");
-	// 		$merged_bib_file_text .= fread($bib_file, filesize("../../pubs/".$bib_file_path));
-	// 		$merged_bib_file_text .= "\n\n";
-	// 		fclose($bib_file);
-	// 	}
-	// } $merged_bib_file = fopen("../../pubs/all.bib", "w");
-	// fwrite($merged_bib_file, $merged_bib_file_text);
-	// fclose($merged_bib_file);
-	// echo("Done!");
-?>
-<!-- </code></pre> -->
-
-<h1>Generate publications</h1>
-<pre><code>
-<?php
-	$bibhtmler = new bibhtmler(array('groupby' => 'class'));
-	$pubs_text = "---\n";
-	$pubs_text .= "layout: page\n";
-	$pubs_text .= "title: Publications\n";
-	$pubs_text .= "permalink: /pubs/\n";
-	$pubs_text .= "---\n\n";
-	$pubs_text .= "{% raw %}{% raw %}{% endraw %}\n";
-	$pubs_text .= "<div class=\"container\">";
-	$pubs_text .= $bibhtmler->process('../../pubs/all.bib');
-	$pubs_text .= "</div>";
-	$pubs_text .= "{% assign openTag = '{%' %}\n";
-	$pubs_text .= "{{ openTag }} endraw %}\n";
-	$pubs_page = fopen("../../pubs/index.html", "w");
-	fwrite($pubs_page, $pubs_text);
-	fclose($pubs_page);
-	echo("Done!");
-?>
-</code></pre>
-
 <h1>Jekyll build</h1>
 <pre><code>
 <?php
